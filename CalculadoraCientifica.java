22744896891f2d455587e20825f5b89378cdeffd
Pedro Henrique <peagah@gmail.com>
2015-06-23 10:18:58 -0400

    metodos implementados

--- CalculadoraCientifica.java
+++ CalculadoraCientifica.java
@@ -6,7 +6,12 @@ public class CalculadoraCientifica {
 	int subtrair(int a, int b){
 		return a- b;
 	}
+	int fatorial(){println("bug Aqui");return 0;}
+	int dividir(){return 0; }
+	int multiplicar(){ return 0; }
+	int sqrt(){return 0;}
+
 	int pow(){
-return 0;
+	return 0;
 }
 }
